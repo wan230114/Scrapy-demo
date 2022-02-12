c0525131427a746e8b548cbd9b130ce1b1bfcd78
wan230114 <1170101471@qq.com>
2022-02-13 00:37:28 +0800

    douban change.

--- douban/douban/settings.py
+++ douban/douban/settings.py
@@ -13,11 +13,17 @@ SPIDER_MODULES = ['douban.spiders']
 NEWSPIDER_MODULE = 'douban.spiders'
 
 
+# # Crawl responsibly by identifying yourself (and your website) on the user-agent
+# #USER_AGENT = 'douban (+http://www.yourdomain.com)'
+
+# # Obey robots.txt rules
+# ROBOTSTXT_OBEY = True
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'douban (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
