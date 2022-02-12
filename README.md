0e1d3b34c239b394a195df2098606716a8b851b1
wan230114 <1170101471@qq.com>
2022-02-13 00:54:29 +0800

    change.

--- README.md
+++ README.md
@@ -5,9 +5,20 @@
 https://book.douban.com/top250
 
 
+## demo 1
+
 ```bash
 scrapy startproject douban
-
+# 编辑好后执行
+scrapy crawl douban
 
 ```
 
+## demo 2
+
+```bash
+scrapy startproject jobui
+
+scrapy crawl jobui
+
+```
--- jobui/jobui/settings.py
+++ jobui/jobui/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'jobui.spiders'
 #USER_AGENT = 'jobui (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -25,7 +25,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -86,3 +86,7 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+FEED_URI = './storage/data/%(name)s.csv'
+FEED_FORMAT = 'csv'
+FEED_EXPORT_ENCODING = 'ansi'
